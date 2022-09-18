a68c1c26470525e38a475270bf796bf3d4989b16
sam60420 <git@github.com:sam60420>
2022-09-18 06:35:58 -0500

    push

---
+++ app.py
@@ -0,0 +1,4 @@
+print("Olumide Gbadebo")
+print('o----')
+print(' ||||')
+print('*' * 10)
---
+++ app2.py
@@ -0,0 +1,5 @@
+price = 10    # numbers
+rating = 4.9    # float
+name = 'Olu'
+is_published = False    # booleans
+print(price)
